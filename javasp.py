0ff804dd3c964296dd5e01491ab96929c811e91d
Silva Joao <Joao.Silva@tridonic.com>
2024-08-23 12:05:03 +0100

    (no message)

--- javasp.py
+++ javasp.py
@@ -177,7 +177,12 @@ class L0Handler:
 
 def parse_whole(source:str):
 
+    parse_by_line(map(lambda match: match.group(0), re.finditer(pattern='(?m)^(.*)$', string=source)))
+
+def parse_by_line(lines:typing.Iterable[str]):
+
     l0 = L0Handler()
-    for line in source.splitlines():
+    for line in lines:
 
         l0.handle_line(line)
+
