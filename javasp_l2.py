5d6a351bd160fe59f3f4536450aec53bab1cb451
Silva Joao <jl95terceira@gmail.com>
2024-09-02 10:19:02 +0100

    (no message)

--- javasp_l2.py
+++ javasp_l2.py
@@ -6,6 +6,10 @@ def _access_name(access:model.AccessModifier):
 
     return access.name if access is not model.AccessModifiers.PACKAGE_PRIVATE else 'package-private'
 
+def _join_args(*aa:str):
+
+    return f'\n{'\n  '.join(('', *aa))}\n'
+
 class L2Handler: 
 
     def handle_package           (self, name:str): 
@@ -14,7 +18,7 @@ class L2Handler:
 
     def handle_import            (self, name:str,static:bool): 
         
-        print(f'Handling{' static' if static else ''} import: {name}')
+        print(f'Handling import: {name}{_join_args(f'{static    =}')}')
         
     def handle_annotation        (self, name:str): 
         
@@ -22,7 +26,12 @@ class L2Handler:
 
     def handle_class             (self, name:str, static:bool, access:model.AccessModifier, finality:model.FinalityType, type:model.ClassType, extends:str|None, implements:list[str]): 
         
-        print(f'Handling {'' if finality is model.FinalityTypes.DEFAULT else finality.name}{'' if not static else ' static'} {type.name}: {name} - {access.name if access is not model.AccessModifiers.PACKAGE_PRIVATE else 'package-private'} access, extends {repr(extends)}, implements {repr(implements)}')
+        print(f'Handling class: {name}{_join_args(f'{access    =}',
+                                                  f'{static    =}',
+                                                  f'{finality  =}',
+                                                  f'{type      =}',
+                                                  f'{extends   =}',
+                                                  f'{implements=}')}')
 
     def handle_class_end         (self): 
 
@@ -30,16 +39,26 @@ class L2Handler:
 
     def handle_static_constructor(self, body:str):
 
-        print(F'Handling static constructor\n  Body: {repr(body)}')
+        print(F'Handling static constructor{_join_args(f'{body      =}')}')
 
     def handle_constructor       (self, args:dict[str,model.Argument], body:str):
 
-        print(f'Handling constructor with args {repr(args)}\n  Body: {repr(body)}')
+        print(f'Handling constructor:{_join_args(f'{args      =}',
+                                                 f'{body      =}')}')
 
     def handle_attr              (self, name:str, static:bool, access:model.AccessModifier, final:bool, type_name:str, value:str|None):
 
-        print(f'Handling declaration of {repr(name)} as {_access_name(access)}{' final' if final else ''} {repr(type_name)}{f'\n  = {value}' if value is not None else ''}')
+        print(f'Handling attribute: {name}{_join_args(f'{access    =}',
+                                                      f'{static    =}',
+                                                      f'{final     =}',
+                                                      f'{type_name =}',
+                                                      f'{value     =}')}')
 
     def handle_method            (self, name:str, static:bool, access:model.AccessModifier, finality:model.FinalityType, type_name:str, args:dict[str,model.Argument], body:str|None):
 
-        print(f'Handling declaration of {_access_name(access)}{' static' if static else ''}{'' if finality is model.FinalityTypes.DEFAULT else f' {finality.name}'} method {repr(name)} with arguments ({','.join(f'{'' if not v.final else 'final '}{v.type_name} {repr(a)}' for a,v in args.items())}) -> {type_name}\n  {f'Body: {repr(body)}' if body is not None else 'No body'}')
+        print(f'Handling method: {name}{_join_args(f'{access    =}',
+                                                   f'{static    =}',
+                                                   f'{finality  =}',
+                                                   f'{type_name =}',
+                                                   f'{args      =}',
+                                                   f'{body      =}')}')
