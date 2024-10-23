0d13fb573f069ba8261a3d135678dceed8f99692
Silva Joao <jl95terceira@gmail.com>
2024-10-23 10:46:42 +0100

    (no message)

---
+++ project/tests/java_files/Test2.java
@@ -0,0 +1,9 @@
+package project.tests.java_files;
+
+import java.util.*;
+
+public class Test2 {
+    
+    public void Test2(@QueryParam List<@NonNull Integer> ints) {throw new Exception(){};}
+    public      Test2(@QueryParam List<@NonNull Integer> ints) {throw new Exception(){};}
+}
